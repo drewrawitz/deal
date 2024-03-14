b7d34800466b988a485d345ce2b3d7a9a60c440b
Drew Rawitz <email@drewrawitz.com>
2024-03-14 13:58:35 -0400

    scroll to bottom of each chat message

--- apps/web/src/components/Chat.tsx
+++ apps/web/src/components/Chat.tsx
@@ -2,7 +2,7 @@ import { classNames, getAvatarUrl, relativeDate } from "@deal/utils-client";
 import CommentInput from "./CommentInput";
 import { useChatMessagesQuery } from "@deal/hooks";
 import { config } from "@deal/utils-client";
-import { useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { socket } from "../socket";
 import { IconCrown } from "@tabler/icons-react";
 
@@ -12,6 +12,7 @@ interface ChatProps {
 
 export default function Chat(props: ChatProps) {
   const { gameId } = props;
+  const scrollRef = useRef<HTMLDivElement>(null);
   const { data: chat, refetch } = useChatMessagesQuery({
     take: 50,
     ...(gameId && {
@@ -19,14 +20,26 @@ export default function Chat(props: ChatProps) {
     }),
   });
 
+  const scrollToBottom = () => {
+    setTimeout(() => {
+      if (scrollRef.current) {
+        scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+      }
+    }, 100);
+  };
+
   useEffect(() => {
     const channel = gameId ? `game.${gameId}.chat` : "message.created";
 
     function onNewChat() {
       refetch();
+      scrollToBottom();
     }
 
     socket.on(channel, onNewChat);
+
+    // Scroll to bottom on initial component load
+    scrollToBottom();
 
     return () => {
       socket.off(channel, onNewChat);
@@ -35,7 +48,7 @@ export default function Chat(props: ChatProps) {
 
   return (
     <>
-      <div className="max-h-[500px] overflow-y-auto">
+      <div ref={scrollRef} className="max-h-[500px] overflow-y-auto">
         {chat?.count === 0 && (
           <p className="text-sm mb-4 text-center">
             There are no chat messages. Be the first one to chime in!
